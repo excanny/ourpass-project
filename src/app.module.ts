c52a9e59c0f5a3947dee89629c2ff65bf6cc3289
Godson <godson.ihemere>
2022-08-31 16:50:28 +0100

    added entity class and codes to user service class and typeorm integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'test2',
+      entities: [User],
+      synchronize: true,
+    }),AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
