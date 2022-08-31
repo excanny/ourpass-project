ffa12df7174596b4f5efaa4c4ca8136f493c1d21
Godson <godson.ihemere>
2022-08-31 22:25:09 +0100

    added seeding for users and create user function

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from './user.entity'
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from '../dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -9,5 +11,39 @@ export class UsersService {
 
   findOne(username: string): Promise<User> {
     return this.usersRepository.findOneBy({ username });
+  }
+
+  async create(userDto: CreateUserDto): Promise<User> {
+    const user = new User();
+    user.username = userDto.username;
+    user.password = userDto.password;
+    return this.usersRepository.save(user)
+  }
+
+  async encryptPassword(password:string) {
+    const salt = await bcrypt.genSalt()
+    return await bcrypt.hash(password, salt)
+  }
+
+  async onModuleInit() {
+
+    var encryptedPassword1 =  await this.encryptPassword('john@gmail.com');
+    var encryptedPassword2 =  await this.encryptPassword('ken@gmail.com');
+    try {
+        const usersData = [{
+          id: 1,
+          username: 'John',
+          password: encryptedPassword1,
+        },
+        {
+          id: 2,
+          username: 'Ken',
+          password: encryptedPassword2,
+        
+        }
+      ];
+        const user = await this.usersRepository.save(usersData);
+        //console.log(user);
+    } catch (error) {throw error;}
   }
 }
